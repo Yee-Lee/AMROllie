64ff19ea0d2fffd6822fbed7be15b09c8c982b14
Yee-Lee <yeelee.tw@gmail.com>
2026-05-03 19:01:14 +0800

    host: fix uart test and symbolic device path
    /dev/ollie-core for the uart communication to ESP

--- host/micro_ros/run_docker.sh
+++ host/micro_ros/run_docker.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
- docker run -it --rm   --net=host --privileged   -v /dev:/dev   -e ROS_DOMAIN_ID=30   microros/micro-ros-agent:humble serial --dev /dev/serial0 -b 460800
+
+docker run -it --rm \
+    --name micro_ros_agent \
+    --privileged \
+    --net=host \
+    -e ROS_DOMAIN_ID=30 \
+    -v /dev:/dev \
+    microros/micro-ros-agent:humble \
+    serial --dev /dev/ollie_core -b 460800
